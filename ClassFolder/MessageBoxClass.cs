1cabbb1cb49a7f1f59ee90d81051dccc3c17fcac
Алексей <@DESKTOP-IKD2985>
2022-05-11 18:59:31 +0300

    Добавьте файлы проекта.

---
+++ ClassFolder/MessageBoxClass.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace MyRecipesBatsulya.ClassFolder
+{
+    class MessageBoxClass
+    {
+        public static void ErrorMessageBox(string error)
+        {
+            MessageBox.Show(error, "Ошибка", MessageBoxButton.OK,
+                MessageBoxImage.Error);
+        }
+        public static void ErrorMessageBox(Exception error)
+        {
+            MessageBox.Show(error.Message, "Ошибка", MessageBoxButton.OK,
+                MessageBoxImage.Error);
+        }
+        public static void InfoMessageBox(string text)
+        {
+            MessageBox.Show(text, "Информация", MessageBoxButton.OK,
+                MessageBoxImage.Information);
+        }
+        public static bool QuestuonMessageBox(string question)
+        {
+            return MessageBoxResult.Yes  == MessageBox.Show(question, "Вопрос",
+               MessageBoxButton.YesNo, MessageBoxImage.Question);
+        }
+        public static void ExitMessageBox()
+        {
+            if (QuestuonMessageBox("Хотите выйти?"))
+            {
+                App.Current.Shutdown();
+            }
+        }
+    }
+}
---
+++ WindowFolder/MainWindow.xaml.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using MyRecipesBatsulya.ClassFolder;
+
+namespace MyRecipesBatsulya.WindowFolder
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            MainFrame.Navigate(new PageFolder.ListIngredientPage());
+        }
+
+        private void BtnDish_Click(object sender, RoutedEventArgs e)
+        {
+            MainFrame.Navigate(new PageFolder.ListDishPage());
+        }
+
+        private void BtnIngredients_Click(object sender, RoutedEventArgs e)
+        {
+            MainFrame.Navigate(new PageFolder.ListIngredientPage());
+        }
+
+        private void BtnExit_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBoxClass.ExitMessageBox();
+        }
+    }
+}
