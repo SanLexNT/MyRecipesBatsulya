c264fe8b5ceb2f01085a9e97746b17314a63505b
Alexey <batsulyaalexey@gmail.com>
2022-05-13 11:53:53 +0300

    Изменение выборки комбобокса и поиска в текстбоксе

--- PageFolder/ListDishPage.xaml.cs
+++ PageFolder/ListDishPage.xaml.cs
@@ -13,6 +13,7 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 using MyRecipesBatsulya.DataFolder;
+using MyRecipesBatsulya.ClassFolder;
 
 namespace MyRecipesBatsulya.PageFolder
 {
@@ -24,23 +25,44 @@ namespace MyRecipesBatsulya.PageFolder
         public ListDishPage()
         {
             InitializeComponent();
-            LvDishes.ItemsSource =
-                DBEntities.GetContext().Dish.ToList();
+            LoadData();
         }
 
         private void CbCategory_SelectionChanged(object sender, SelectionChangedEventArgs e)
         {
-
+            if (!(CbCategory.SelectedItem is Category category))
+            {
+                return;
+            }
+            else
+            {
+                LvDishes.ItemsSource =
+                        DBEntities.GetContext().Dish.ToList()
+                        .Where(d => d.Category.Name == category.Name);
+            }
         }
 
         private void TbSearch_TextChanged(object sender, TextChangedEventArgs e)
         {
-
+            LvDishes.ItemsSource =
+                        DBEntities.GetContext().Dish.Where
+                        (d => d.Name.StartsWith(TbSearch.Text)).ToList();
+            if(LvDishes.Items.Count <=0)
+                MessageBoxClass.ErrorMessageBox("Блюдо не найдено");
+            
         }
 
         private void Grid_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
         {
 
+        }
+
+        private void LoadData()
+        {
+            LvDishes.ItemsSource =
+                DBEntities.GetContext().Dish.ToList();
+            CbCategory.ItemsSource =
+                DBEntities.GetContext().Category.ToList();
         }
     }
 }
